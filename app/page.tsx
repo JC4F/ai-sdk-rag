f8c8aa879677bc7fc51e7a8c01dd4c9c054a61a3
jc4f <lequan12122002@gmail.com>
2024-09-30 11:16:39 +0700

    add resource tool

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
-'use client';
+"use client";
 
-import { useChat } from 'ai/react';
+import { useChat } from "ai/react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <div className="space-y-4">
-        {messages.map(m => (
+        {messages.map((m) => (
           <div key={m.id} className="whitespace-pre-wrap">
             <div>
               <div className="font-bold">{m.role}</div>
-              <p>{m.content}</p>
+              <p>
+                {m.content.length > 0 ? (
+                  m.content
+                ) : (
+                  <span className="italic font-light">
+                    {"calling tool: " + m?.toolInvocations?.[0].toolName}
+                  </span>
+                )}
+              </p>
             </div>
           </div>
         ))}
